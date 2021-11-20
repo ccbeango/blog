3b5762629763f30ddd3705b84c44a3085c7c9185
Andy <liuyh940@gmail.com>
2021-11-20 16:34:41 +0800

    添加评论

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -70,26 +70,26 @@ module.exports = [
   //     hm: '503f098e7e5b3a5b5d8c5fc2938af002',
   //   },
   // ],
-  // [
-  //   'vuepress-plugin-comment', // 评论
-  //   {
-  //     choosen: 'gitalk',
-  //     options: {
-  //       clientID: 'a6e1355287947096b88b',
-  //       clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-  //       repo: 'blog-gitalk-comment', // GitHub 仓库
-  //       owner: 'xugaoyi', // GitHub仓库所有者
-  //       admin: ['xugaoyi'], // 对仓库有写权限的人
-  //       // distractionFreeMode: true,
-  //       pagerDirection: 'last', // 'first'正序 | 'last'倒序
-  //       id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-  //       title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-  //       labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-  //       body:
-  //         '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
-  //     },
-  //   },
-  // ],
+  [
+    'vuepress-plugin-comment', // 评论
+    {
+      choosen: 'gitalk',
+      options: {
+        clientID: '89d88ee37c9a43f5e6ab',
+        clientSecret: 'c2ccc3c2eaedcb8ca41e9722677d810283363e6e',
+        repo: 'blog', // GitHub 仓库
+        owner: 'ccbeango', // GitHub仓库所有者
+        admin: ['ccbeango'], // 对仓库有写权限的人
+        // distractionFreeMode: true,
+        pagerDirection: 'last', // 'first'正序 | 'last'倒序
+        id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
+        title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
+        labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
+        body:
+          '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+      },
+    },
+  ],
   [
     '@vuepress/last-updated', // "上次更新"时间格式
     {
